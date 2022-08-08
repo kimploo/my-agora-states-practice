0b8e21053c84c8d1679376177e7a194171c06d5f
Hong Shik Branden Kim <kimploo@gmail.com>
2022-08-08 17:59:06 +0900

    fix: better test

--- my-agora-states-client/src/index.test.js
+++ my-agora-states-client/src/index.test.js
@@ -6,8 +6,9 @@ import App from './App';
 describe('Bare minimum requirement', () => {
   describe('App.js 컴포넌트 유닛 테스트', () => {
     test('H1 엘리먼트에 "My Agora States" 텍스트 노드가 있어야 합니다.', () => {
-      const { queryByText } = render(<App />)
-      expect(queryByText(/My Agora States/)).toBeTruthy();
+      const { container } = render(<App />)
+      const h1 = container.querySelector('h1');
+      expect(h1.textContent).toBe('My Agora States');
     });
   });
 });
